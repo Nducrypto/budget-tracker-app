68a6497d580b30f353b0a34bda56129035cf82b4
nducrypto <agbonud9@gmail.com>
2022-08-15 16:22:07 +0100

    second

--- client/src/api/Index.js
+++ client/src/api/Index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API = axios.create({ baseURL: "https://ndu-budget.herokuapp.com/api" });
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: "https://ndu-budget.herokuapp.com/api" });
+// const API = axios.create({ baseURL: "http://localhost:5000/api" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
